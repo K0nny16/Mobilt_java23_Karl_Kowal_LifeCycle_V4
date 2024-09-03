478e455b997030236b0008dfa1e43d699509403f
Karl Kowal <kowal.karl99@gmail.com>
2024-09-03 20:57:23 +0200

    batman

---
+++ app/src/main/java/com/example/mobilt_java23_karl_kowal_lifecycle_v4/FirebaseManager.kt
@@ -0,0 +1,25 @@
+package com.example.mobilt_java23_karl_kowal_lifecycle_v4
+
+import android.app.Activity
+import com.google.firebase.auth.FirebaseAuth
+
+
+class FirebaseManager(private val activity: Activity) {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance();
+
+    //Görs Async med onComplete. Alltså det som finns i onComplete är det som returneras.
+    //Unit är kotlins motsvarighet till Void.
+    fun signIn(email: String,password:String, onComplete:(Boolean,String?) -> Unit){
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(activity){ task ->
+                if(task.isSuccessful){
+                    onComplete(true,null)
+                }else{
+                    //Hämtar felmedelandet ifall det finns annars sätter vi ett custom.
+                    val errorMessage = task.exception?.message?:"Unknown Error!"
+                    onComplete(false,errorMessage)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/mobilt_java23_karl_kowal_lifecycle_v4/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.mobilt_java23_karl_kowal_lifecycle_v4
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //Initierar UI-element.
+        val emailText = findViewById<EditText>(R.id.email);
+        val pwText = findViewById<EditText>(R.id.password);
+        val loginButton = findViewById<Button>(R.id.loginButton);
+
+
+        loginButton.setOnClickListener {
+            val email = emailText.text.toString();
+            val pw = pwText.text.toString();
+            //Kollar så alla fields är ifylda. (Kanske kan göras direkt på komponenten?)
+            if(pw.isNotEmpty() && email.isNotEmpty()){
+                //Initierar firebase innan den behövs för att skicka infon.
+                val firebaseManager = FirebaseManager(this);
+                firebaseManager.signIn(email,pw){success,errorMsg ->
+                    if(success){
+                        Toast.makeText(this,"Login successful!",Toast.LENGTH_SHORT).show()
+                        //Byta activity.
+                    }else{
+                        Toast.makeText(this,"Error: $errorMsg",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Please fill out all fields!",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
